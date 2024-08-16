960d2e45d945135d7a21affae10c4dd7c3f81fed
Jan de Janger <jandejager10@hotmail.com>
2024-08-16 12:50:39 +0000

    number 2

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 """
 Django settings for django_todo project.
 
@@ -74,11 +80,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.parse(os.environ.get('postgres://u6wftz21rdn:qrDHMfUE5XAb@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/mom_cheek_image_251078'))
 }
 
 
